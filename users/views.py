4440eb30bae5028c7a0aa0ddb0591a8764a74952
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-23 10:16:11 +0300

    Added profile view funtion

--- users/views.py
+++ users/views.py
@@ -18,3 +18,19 @@ def register(request):
     return render(request, 'users/registration.html', {"form":form})
 
 
+def profile(request):
+    profile = Profile.objects.get_or_create(user=request.user)
+    if request.method =='POST':
+        profile_update_form = ProfileUpdateForm(request.POST or None, request.FILES, instance=request.user.profile)
+        if profile_update_form.is_valid():
+            profile_update_form.save()
+            return redirect('profile')
+    else:
+        profile_update_form = ProfileUpdateForm()
+
+    context = {
+        'profile_update_form': profile_update_form,
+       
+    }
+    return render(request, 'users/profile.html', context)
+
