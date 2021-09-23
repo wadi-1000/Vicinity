229a87a531dfb0bd7be75501734d8d2faa79de84
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-23 09:11:52 +0300

    Created profile model for user profiles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from hood.models import Neighbourhood
 # Create your models here.
+class Profile(models.Model):
+    name = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    image = CloudinaryField(default = 'default.jpeg')
+    bio = models.TextField( default="Please Update Bio")
+    hood=models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,null=True)
+    number = models.IntegerField(default =0)
+    email = models.EmailField(default = "Enter your valid email address")
+
+    def __str__(self):
+        return '{} {}'.format(self.image.url, self.bio)
+
+
+    def save_profile(self):
+        self.save()
