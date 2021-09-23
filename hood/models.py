661ca743d9099ca24ab605d203d76a5f07b7a0ad
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-23 09:58:10 +0300

    Created neighbourhood model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import User
+from cloudinary.models import CloudinaryField
+from users.models import Profile
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    hood = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    occupants = models.PositiveIntegerField(default = 0)
+    
+    
