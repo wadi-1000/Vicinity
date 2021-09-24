286b304e7f646f6671f495569ace52a4add96fed
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-24 14:36:07 +0300

    Added function to upload a hood

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from django.htpp import HttpResponse
-
+from models import Neighbourhood
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+@login_required
+def uploadNeighbourhood(request):
+    form=UploadNewNeighbourhood()
+    current_user=request.user
+
+    if request.method =="POST":
+        form=UploadNewProject(request.POST, request.FILES)
+        if form.is_valid():
+            hood=form.save(commit=False)
+            hood.user=current_user
+            hood.save()
+
+        return redirect('home')
+
+    else:
+        form=UploadNewNeighbourhood()
+
+    return render(request, 'uploadhood.html', {"form":form})
