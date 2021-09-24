66e2b68a0df3a340dbeb58997109c3479a63d537
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-24 14:17:18 +0300

    Added more fesutures to the hood module

--- hood/models.py
+++ hood/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from users.models import Profile
+
 
 # Create your models here.
 class Neighbourhood(models.Model):
     hood = models.CharField(max_length = 100)
     location = models.CharField(max_length = 100)
-    occupants = models.PositiveIntegerField(default = 0)
-    
+    pic = CloudinaryField('image')
+    description = models.TextField()
+    hospital_number = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+    occupant_count = models.IntegerField(null=True, blank=True)
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hood')
     def create_neighbourhood(self):
         self.save()
     
     def delete_neighbourhood(self):
         self.delete()
-        
