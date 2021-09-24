b1fa8dbfe844a3f510add6ad4983ddda4737c44d
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-24 17:44:45 +0300

    Added to path to view hood

--- hood/urls.py
+++ hood/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = 'home'),
-    path('add_hood/',views.uploadNeighbourhood, name = 'add_hood')
+    path('add_hood/',views.uploadNeighbourhood, name = 'add_hood'),
+    path('viewhood/',views.viewhood, name = 'viewhood'),
 ]
