2efe4ed9cf6c7d3948f2047d90203438be0dca3b
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-25 06:58:11 +0300

    Added path to view a single hood

--- hood/urls.py
+++ hood/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name = 'home'),
     path('add_hood/',views.uploadNeighbourhood, name = 'add_hood'),
-    path('viewhood/',views.viewhood, name = 'viewhood'),
+    path('viewhood/',views.viewHood, name = 'viewhood'),
+    path('hood/<int:pk>/',views.hood, name = 'hood'),
 ]
