235a534e85d90a8e26c6668199ec31091d5c55ae
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-26 10:42:02 +0300

    Added post model

--- hood/models.py
+++ hood/models.py
@@ -13,11 +13,19 @@ class Neighbourhood(models.Model):
     police_number = models.IntegerField(null=True, blank=True)
     occupant_count = models.IntegerField(null=True, blank=True)
     admin = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+            return ( self.hood)
     def create_neighbourhood(self):
         self.save()
     
     def delete_neighbourhood(self):
         self.delete()
+
+    @classmethod
+    def find_neighbourhood(cls, neighbourhood_id):
+        return cls.objects.filter(id=neighbourhood_id)
+
 
 
 class Buisness(models.Model):
@@ -25,12 +33,28 @@ class Buisness(models.Model):
    owner = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE,null=True)
    image = CloudinaryField('image')
+   description = models.TextField()
    email = models.EmailField(default = "Please put in your buisness email address")
 
-    def create_buisness(self):
+   def create_buisness(self):
         self.save()
     
-    def delete_buisness(self):
+   def delete_buisness(self):
         self.delete()
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} Post'
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
