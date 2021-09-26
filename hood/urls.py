75628d34a21ee6b7a36b6939f2ac4e44e382c4f7
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-26 11:58:05 +0300

    Added path to view posts

--- hood/urls.py
+++ hood/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('hood/<int:pk>/',views.hood, name = 'hood'),
     path('add_bizna/',views.uploadBuisness, name = 'add_bizna'),
     path('bizna/',views.viewBizna, name = 'view_bizna'),
+    path('viewbizna/<int:pk>/',views.bizna, name = 'bizna'),
+    path('post/',views.create_post, name = 'post'),
+    path('posts/',views.viewPost, name = 'posts')
 ]
