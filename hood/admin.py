2cd229c865c46659879c11b37372195ac8ef79ba
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-09-26 10:44:26 +0300

    Added post model to admin site

--- hood/admin.py
+++ hood/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Neighbourhood,Buisness
+from .models import Neighbourhood,Buisness,Post
 
 # Register your models here.
 admin.site.register(Neighbourhood)
 admin.site.register(Buisness)
+admin.site.register(Post)
